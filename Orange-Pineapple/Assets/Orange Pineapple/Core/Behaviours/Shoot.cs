07ea08e254cb9bf8d504fb5442b113ceb68e5d32
SnpM <jpthek9@gmail.com>
2015-12-26 21:46:06 -0700

    Update LS, add UFPS, start on Shoot behaviour

---
+++ Orange-Pineapple/Assets/Orange Pineapple/Core/Behaviours/Shoot.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using Lockstep;
+
+namespace OrangePineapple
+{
+    public class Shoot : Ability
+    {
+        [SerializeField,FixedNumber]
+        private long _firePeriod;
+        protected override void OnSimulate()
+        {
+            base.OnSimulate();
+        }
+        protected override void OnVisualize()
+        {
+            if (Input.GetMouseButtonDown(0)) {
+            }
+        }
+    }
+}
