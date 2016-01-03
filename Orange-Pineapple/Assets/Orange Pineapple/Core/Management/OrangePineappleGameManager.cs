322ee20da50a2aa58f393bdaadb0868de7f4d31b
SnpM <jpthek9@gmail.com>
2016-01-03 11:18:31 -0700

    Update

--- Orange-Pineapple/Assets/Orange Pineapple/Core/Management/OrangePineappleGameManager.cs
+++ Orange-Pineapple/Assets/Orange Pineapple/Core/Management/OrangePineappleGameManager.cs
@@ -7,14 +7,7 @@ namespace OrangePineapple
     [RequireComponent (typeof (FPSHelper))]
     public class OrangePineappleGameManager : GameManager
     {
-        private NetworkHelper _networkHelper;
-        public override NetworkHelper MainNetworkHelper
-        {
-            get
-            {
-                return _networkHelper ?? (_networkHelper = GetComponent<PhotonNetworkHelper>());
-            }
-        }
+      
         private InterfacingHelper _interfacingHelper = new FPSInterfacingHelper();
         public override InterfacingHelper MainInterfacingHelper
         {
