7260ecbb8cbdaeb996b54f8d421e7ca639defcaf
SnpM <jpthek9@gmail.com>
2016-01-02 17:21:53 -0700

    Add Photon and rotation clamping

--- Orange-Pineapple/Assets/Orange Pineapple/Core/Interfacing/FirstPersonCamera.cs
+++ Orange-Pineapple/Assets/Orange Pineapple/Core/Interfacing/FirstPersonCamera.cs
@@ -2,7 +2,8 @@
 using System.Collections;
 using UnityStandardAssets.CrossPlatformInput;
 
-public class FirstPersonCamera : MonoBehaviour {
+public class FirstPersonCamera : MonoBehaviour
+{
     [SerializeField]
     float XSensitivity = 2;
     [SerializeField]
@@ -11,27 +12,88 @@ public class FirstPersonCamera : MonoBehaviour {
     bool smooth = true;
     [SerializeField]
     float smoothTime = 1f;
-	// Use this for initialization
-	void Start () {
+
+    [SerializeField]
+    float minVertical = -80f;
+    [SerializeField]
+    float maxVertical = 80f;
+    // Use this for initialization
+    void Start()
+    {
         targetRot = transform.rotation;
-	}
+    }
+
     Quaternion targetRot;
-	// Update is called once per frame
-	void Update () {
+    // Update is called once per frame
+    void Update()
+    {
         float yRot = CrossPlatformInputManager.GetAxis("Mouse X") * XSensitivity;
         float xRot = CrossPlatformInputManager.GetAxis("Mouse Y") * YSensitivity;
 
         Vector3 eulers = targetRot.eulerAngles;
-        targetRot = Quaternion.Euler(eulers.x + -xRot, eulers.y + yRot, 0);
-        if(smooth)
+        
+        targetRot = Quaternion.Euler(ClampAngle(eulers.x + -xRot, minVertical, maxVertical), eulers.y + yRot, 0);
+
+        if (smooth)
         {
-            transform.rotation = Quaternion.Lerp (transform.localRotation, targetRot,
+            transform.rotation = Quaternion.Lerp(transform.localRotation, targetRot,
                 smoothTime * Time.deltaTime);
-        }
-        else
+        } else
         {
             transform.rotation = targetRot;
 
         }
-	}
+    }
+
+    public float ClampAngle(float angle, float min, float max)
+    {
+        bool minMoreThan = min > max;
+        if (min < 0)
+        {
+            min = WrapAngle(min);
+            if (min > max)
+                minMoreThan = true;
+        } else
+            min = WrapAngle(min);
+
+        max = WrapAngle(max);
+        angle = WrapAngle(angle);
+
+        if (minMoreThan == false)
+        {
+            if (angle < min || angle > max)
+                angle = ClosestAngle (angle,min, max);
+            
+        } else
+        {
+
+            if (angle < min && angle > max) {
+                angle = ClosestAngle (angle, min, max);
+            }
+        }
+        return angle;
+    }
+
+    float ClosestAngle (float angle, float val1, float val2) {
+        float dist1 = Mathf.Min (Mathf.Abs(angle - val1), Mathf.Abs(360 - val1 + angle));
+        float dist2 = Mathf.Min (Mathf.Abs(angle - val2), Mathf.Abs(360 - val2 + angle));
+        return (dist1 < dist2 ? val1 : val2);
+    }
+
+    //Make sure angle is within 0,360 range
+    float WrapAngle(float angle)
+    {
+        //If its negative rotate until its positive
+        if (angle < 0)
+        {
+            angle += 360;
+            while (angle < 0)
+                angle += 360;
+        }
+        else if (angle > 360)
+        {
+            angle %= 360;
+        }
+        return angle;
+    }
 }
--- Orange-Pineapple/Assets/Orange Pineapple/Core/Management/OrangePineappleGameManager.cs
+++ Orange-Pineapple/Assets/Orange Pineapple/Core/Management/OrangePineappleGameManager.cs
@@ -1,18 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
 using Lockstep;
-
+using Lockstep.NetworkHelpers;
 namespace OrangePineapple
 {
     [RequireComponent (typeof (FPSHelper))]
     public class OrangePineappleGameManager : GameManager
     {
-        private NetworkHelper _networkHelper = new Lockstep.Example.ExampleNetworkHelper ();
+        private NetworkHelper _networkHelper;
         public override NetworkHelper MainNetworkHelper
         {
             get
             {
-                return _networkHelper;
+                return _networkHelper ?? (_networkHelper = GetComponent<PhotonNetworkHelper>());
             }
         }
         private InterfacingHelper _interfacingHelper = new FPSInterfacingHelper();
